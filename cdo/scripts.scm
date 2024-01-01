86728b44146297cde7a7ad33dfe7ff910d4fc564
Christina O'Donnell <cdo@mutix.org>
2024-01-01 21:51:29 +0000

    Initial file for scripts

---
+++ cdo/scripts.scm
@@ -0,0 +1,11 @@
+(define-module (cdo scripts)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages python)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix packages)
+  #:use-module (guix build-system trivial))
+
+
